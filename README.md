fe431d6cc16c2c9193d08a4a7353fa983825eec7
Quyen <maelphstro@gmail.com>
2026-05-12 18:48:18 -0700

    YAY MAYBE WORKING ALMOST

--- README.md
+++ README.md
@@ -1,5 +1,12 @@
 ## References
 - https://racheldebarros.com/how-to-use-fastled-with-arduino-to-program-led-strips/
 
-## To run lights
-- run `source venv/bin/activate`
+## For running Microros
+- refer to https://www.hackster.io/514301/micro-ros-on-esp32-using-arduino-ide-1360ca 
+- source the jazzy version instead of humble
+
+## TO DO
+- add publisher to micro-ros_subscriber, make sure is publishing
+- seems lights should be good to go
+- remove venv probably
+
---
+++ build/micro_ros_msgs/micro_ros_msgs__rs/CMakeFiles/micro_ros_msgs__rs.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "/home/quyent/Cyclone/Techno-Luminescence/build/micro_ros_msgs/rosidl_generator_rs/micro_ros_msgs/rust/Cargo.toml"
+  "/home/quyent/Cyclone/Techno-Luminescence/build/micro_ros_msgs/rosidl_generator_rs/micro_ros_msgs/rust/build.rs"
+  "/home/quyent/Cyclone/Techno-Luminescence/build/micro_ros_msgs/rosidl_generator_rs/micro_ros_msgs/rust/src/lib.rs"
+  "/home/quyent/Cyclone/Techno-Luminescence/build/micro_ros_msgs/rosidl_generator_rs/micro_ros_msgs/rust/src/msg.rs"
+  "/home/quyent/Cyclone/Techno-Luminescence/build/micro_ros_msgs/rosidl_generator_rs/micro_ros_msgs/rust/src/msg/rmw.rs"
+  "CMakeFiles/micro_ros_msgs__rs"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/micro_ros_msgs__rs.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
