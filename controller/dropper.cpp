946dd66f71873088a61c5d3162c2f8fce1b37f65
PuffyDucks <26389964+PuffyDucks@users.noreply.github.com>
2026-07-15 02:32:04 -0700

    jusiewjlkemnjuifejwlkqemnfidsoajlkemfdisufjklm,s

--- controller/dropper.cpp
+++ controller/dropper.cpp
@@ -4,24 +4,31 @@
 static Servo dropperServo;
 static unsigned long dropperStartTime;
 static bool dropperActive;
+static int cmd = 0;
 
 void dropper_setup() {
   dropperServo.attach(SERVO_PIN);
   dropperServo.write(RESET_ANGLE);
 }
 
-void dropper_response(int cmd) {
-  if (cmd != 1 && cmd != 2) return; // exits if invalid message
-
-  dropperServo.write(cmd == 1 ? RELEASE_ANGLE_1 : RELEASE_ANGLE_2);
+void dropper_response(int msg_cmd) {
+  if (msg_cmd != 1 && msg_cmd != 2) return; // exits if invalid message
   dropperStartTime = millis();
   dropperActive = true;
+  cmd = msg_cmd;
 }
 
 void dropper_update() {
   if (!dropperActive) return;
-  if (millis() - dropperStartTime < DROPPER_DURATION_MS) return;
-  
-  dropperServo.write(RESET_ANGLE);
-  dropperActive = false;
+  unsigned long dropperActiveTime = millis() - dropperStartTime;
+  int release_angle = (cmd == 1) ? RELEASE_ANGLE_1 : RELEASE_ANGLE_2;
+
+  if (dropperActiveTime > DROPPER_DURATION_MS) {
+    dropperServo.write(RESET_ANGLE);
+    dropperActive = false;
+    return;
+  }
+
+  // alternate every 1000 ms
+  dropperServo.write((dropperActiveTime % 2000 < 1000) ? release_angle : RESET_ANGLE);
 }
--- controller/dropper.h
+++ controller/dropper.h
@@ -2,9 +2,9 @@
 
 //indexed angles that the servo should move to:
 const int SERVO_PIN       = 12;
-const int RESET_ANGLE     = 78;
-const int RELEASE_ANGLE_1 = 90;
-const int RELEASE_ANGLE_2 = 66;
+const int RESET_ANGLE     = 90;
+const int RELEASE_ANGLE_1 = 102;
+const int RELEASE_ANGLE_2 = 78;
 
 const int DROPPER_DURATION_MS = 2000;
 
